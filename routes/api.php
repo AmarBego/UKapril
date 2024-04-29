cb993cc875df4ce50828c6c7deee628d40eb00b2
Amar Begovic <amar.begovic123@hotmail.com>
2024-04-29 20:44:19 +0200

    API fix

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('/')->group(function () {
+
+    Route::get('/bikes', function () {
+        $pdo = new PDO('mysql:host=localhost;dbname=laravel', 'root', '');
+        $statement = $pdo->query('SELECT * FROM bike');
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    });
+
+    Route::get('/bikes/{id}', function ( $id) {
+        $pdo = new PDO('mysql:host=localhost;dbname=laravel', 'root', '');
+        $statement = $pdo->prepare('SELECT * FROM bike WHERE id = :id');
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    })->whereNumber('id');
+});
